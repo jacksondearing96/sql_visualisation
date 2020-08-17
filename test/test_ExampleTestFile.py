82d2e20327098d086e3acfcadadfd4917c14de93
Jack <jacksondearing96@.gmail.com>
2020-08-17 22:55:52 +0930

    Added configurations for python linter

--- test/test_ExampleTestFile.py
+++ test/test_ExampleTestFile.py
@@ -1,27 +1,26 @@
-# An example file for writing unit tests with the unittest framework
-# in python. All test functions must be prefixed with "test_" for the 
-# command that runs all the tests to find it.
+"""An example file for writing unit tests with the unittest framework
+in python. All test functions must be prefixed with "test_" for the
+command that runs all the tests to find it."""
 
 import unittest
 
 import ExampleClassFile
 
-class test_ExampleClass(unittest.TestCase):
-    # Organise your tests inside a class like this, prefixed with "test_"
+
+class TestExampleClass(unittest.TestCase):
+    """Organise your tests inside a class like this, prefixed with 'test_'."""
 
     def setUp(self):
-        # Use this set up function to initialise anything you
-        # may want available to all your test functions below.
+        """Use this set up function to initialise anything you
+        may want available to all your test functions below."""
         self.example_member = None
 
-
-    def test_ExampleFunction(self):
-        # Test functions must be prefixed with "test_"
-        # Test the ExampleMethod() found in the ExampleClass that 
-        # we are testing. It should just return True.
+    def test_example_function(self):
+        """Test functions must be prefixed with "test_".
+        Test the ExampleMethod() found in the ExampleClass that
+        we are testing. It should just return True."""
         example_object = ExampleClassFile.ExampleClass()
-        self.assertEqual(example_object.ExampleMethod(), True)
+        self.assertEqual(example_object.example_method(), True)
 
-
-    def test_AnotherExampleFunction(self):
+    def test_another_example_function(self):
         self.assertTrue(True)
