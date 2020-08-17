5cbf8079b633900dbd2ce7633ec4c8d9ea38e773
Jack <jacksondearing96@.gmail.com>
2020-08-17 22:54:30 +0930

    Updated eslint configurations for node8-->node10

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,12 @@
-module.exports = { "extends": "airbnb-base" };
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'no-console': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'node_modules/*',
+    },
+  ],
+};
