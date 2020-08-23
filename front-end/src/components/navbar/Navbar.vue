420c06501fb6b67222be781a6a988333653ecd59
Gabriel Piragibe <gabriel.piragibe@hotmail.com>
2020-08-23 16:39:21 +0930

    Vue folder structure and some initial steps to play with d3 library

---
+++ front-end/src/components/navbar/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="navbar">
+        <!-- Navbar -->
+        <nav class="navbar navbar-light navbar-expand-md">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Propic</a>
+                <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Home</a></li>
+                        <li class="nav-item" role="presentation">
+                            <router-link to="/about" tag="li"><a class="nav-link">blabla</a></router-link>
+                        </li>
+                        <li class="nav-item" role="presentation"></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <!-- Navbar -->
+      <Navbar></Navbar>
+      <!-- End Navbar -->
+
+      <!-- Heading -->
+      <div>
+        <h2 class="text-center" style="margin-top: 10px;">Data Lineage Visualisation</h2>
+        <p class="lead text-uppercase text-center" style="font-size: 16px;">BY | SIVT1</p>
+      </div>
+      <!-- End Heading -->
+
+      <!-- Force Directed Graph -->
+      <div class="text-center">
+        <div class="jumbotron">
+          <h3>Base table for {{ baseTable }} tables </h3>
+          <h3>Derived from {{ derivedTable }} tables </h3>
+          <svg>
+            <defs>
+
+              <marker id="m-end" markerWidth="10" markerHeight="10" refX="20" refY="3" orient="auto"
+                markerUnits="strokeWidth">
+                <path d="M0,0 L0,6 L9,3 z"></path>
+              </marker>
+
+              <marker id="m-start" markerWidth="6" markerHeight="6" refX="-20" refY="3" orient="auto"
+                markerUnits="strokeWidth">
+                <rect width="4" height="6"></rect>
+              </marker>
+
+            </defs>
+          </svg>
+
+          <d3-network :net-nodes="nodes" :net-links="links" :options="options" :link-cb="linkInfo"
+            @node-click="nodeInfo" />
+        </div>
+      </div>
+      <!-- End Force Directed Graph -->
+
+      <!-- Data Info -->
+      <div class="text-center">
+        <h1>Table's info down here maybe...</h1>
+      </div>
+    </div>
+    <!--End Data Info -->
+
+    <!-- Footer -->
+    <Footer></Footer>
+    <!-- End Footer -->
+  </div>
+</template>
+
+<script>
+  import D3Network from 'vue-d3-network'
+  import Footer from '@/components/footer/Footer.vue'
+  import Navbar from '@/components/navbar/Navbar.vue'
+
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        nodes: [],
+        links: [],
+        nodeSize: 25,
+        canvas: false,
+        tableLinkInformation: [],
+        baseTable: 0,
+        derivedTable: 0
+      }
+    },
+    computed: {
+      options() {
+        return {
+          nodeLabels: true,
+          size: {
+            w: 1000,
+            h: 500
+          },
+          nodeSize: this.nodeSize,
+          force: 5000,
+          canvas: this.canvas
+        }
+      }
+    },
+    methods: {
+      linkInfo(link) {
+        if (link.index != null) this.tableLinkInformation.push(link)
+
+        link._svgAttrs = {
+          'marker-end': 'url(#m-end)',
+          'marker-start': 'url(#m-start)'
+        }
+        return link
+      },
+      nodeInfo(event, node) {
+        this.baseTable = 0
+        this.derivedTable = 0
+
+
+        this.tableLinkInformation.forEach(element => {
+          if (element.sid == node.id) {
+            this.baseTable += 1
+          }
+
+          if (element.tid == node.id) {
+            this.derivedTable += 1
+          }
+        })
+      }
+    },
+    created() {
+      // Retrieving all nodes from vuex
+      this.nodes = this.$store.getters.allNodes
+
+      // Retrieving all links from vuex
+      this.links = this.$store.getters.allLinks
+    },
+    components: {
+      D3Network,
+      Navbar,
+      Footer
+    }
+  }
+</script>
+
+<style>
+</style>
