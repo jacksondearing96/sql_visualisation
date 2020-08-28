04d451a411727be039451e94217abe8421811738
Gabriel Piragibe <gabriel.piragibe@hotmail.com>
2020-08-28 20:35:12 +0930

    Added a landing page, and split code into more components

---
+++ front-end/src/components/sqlInput/SQLInput.vue
@@ -0,0 +1,88 @@
+<template>
+    <div id="sqlinput" class="container highlight-clean">
+        <div class="intro">
+            <h2 class="text-center">Data Lineage Tool</h2>
+            <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat,
+                pellentesque ut laoreet vitae. </p>
+        </div>
+        <div class="buttons">
+            <!-- <input class="form-control-lg" type="text" style="height: 40px;" placeholder="SQL script"> -->
+            <div class="custom-file">
+                <input type="file" class="custom-file-input" id="inputFile">
+                <label class="custom-file-label" for="inputFile" aria-describedby="inputFile">SQL script</label>
+            </div>
+            <button class="btn btn-primary" type="button">Upload!</button>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'SQLInput'
+    }
+</script>
+<style scoped>
+    #sqlinput {
+        margin-top: 120px;
+    }
+
+    .highlight-clean {
+        color: #313437;
+        background-color: #fff;
+        padding: 50px 0;
+    }
+
+    .highlight-clean p {
+        color: #7d8285;
+    }
+
+    .highlight-clean h2 {
+        font-weight: bold;
+        margin-bottom: 25px;
+        line-height: 1.5;
+        padding-top: 0;
+        margin-top: 0;
+        color: inherit;
+    }
+
+    .highlight-clean .intro {
+        font-size: 16px;
+        max-width: 500px;
+        margin: 0 auto 25px;
+    }
+
+    .highlight-clean .buttons {
+        font-family: Roboto, sans-serif;
+        margin-left: 10px;
+        text-align: center;
+    }
+
+    .highlight-clean .buttons .btn {
+        padding: 16px 32px;
+        margin: 6px;
+        border: none;
+        background: none;
+        box-shadow: none;
+        text-shadow: none;
+        opacity: 0.9;
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 13px;
+        letter-spacing: 0.4px;
+        line-height: 1;
+        outline: none;
+        background-color: #ddd;
+    }
+
+    .highlight-clean .buttons .btn:hover {
+        opacity: 1;
+    }
+
+    .highlight-clean .buttons .btn:active {
+        transform: translateY(1px);
+    }
+
+    .highlight-clean .buttons .btn-primary {
+        background-color: #055ada;
+        color: #fff;
+    }
+</style>
