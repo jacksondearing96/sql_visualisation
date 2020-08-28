868f9e2236369b4a7ec38f06eb45be740574d258
Gabriel Piragibe <gabriel.piragibe@hotmail.com>
2020-08-28 20:35:12 +0930

    Added a landing page, and split code into more components

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -12,39 +12,17 @@
       </div>
       <!-- End Heading -->
 
+
+      <!-- SQL Input -->
+      <SQLInput></SQLInput>
+      <!-- End SQL Input -->
+
+
       <!-- Force Directed Graph -->
-      <div class="text-center">
-        <div class="jumbotron">
-          <h3>Base table for {{ baseTable }} tables </h3>
-          <h3>Derived from {{ derivedTable }} tables </h3>
-          <svg>
-            <defs>
-
-              <marker id="m-end" markerWidth="10" markerHeight="10" refX="20" refY="3" orient="auto"
-                markerUnits="strokeWidth">
-                <path d="M0,0 L0,6 L9,3 z"></path>
-              </marker>
-
-              <marker id="m-start" markerWidth="6" markerHeight="6" refX="-20" refY="3" orient="auto"
-                markerUnits="strokeWidth">
-                <rect width="4" height="6"></rect>
-              </marker>
-
-            </defs>
-          </svg>
-
-          <d3-network :net-nodes="nodes" :net-links="links" :options="options" :link-cb="linkInfo"
-            @node-click="nodeInfo" />
-        </div>
-      </div>
+      <ForceGraph v-if="loading"></ForceGraph>
       <!-- End Force Directed Graph -->
-
-      <!-- Data Info -->
-      <div class="text-center">
-        <h1>Table's info down here maybe...</h1>
-      </div>
     </div>
-    <!--End Data Info -->
+    
 
     <!-- Footer -->
     <Footer></Footer>
@@ -53,73 +31,22 @@
 </template>
 
 <script>
-  import D3Network from 'vue-d3-network'
-  import Footer from '@/components/footer/Footer.vue'
   import Navbar from '@/components/navbar/Navbar.vue'
+  import SQLInput from '@/components/sqlInput/SQLInput.vue'
+  import ForceGraph from '@/components/forceGraph/ForceGraph.vue'
+  import Footer from '@/components/footer/Footer.vue'
 
   export default {
     name: 'Home',
-    data() {
+    data(){
       return {
-        nodes: [],
-        links: [],
-        nodeSize: 25,
-        canvas: false,
-        tableLinkInformation: [],
-        baseTable: 0,
-        derivedTable: 0
+        loading: false
       }
-    },
-    computed: {
-      options() {
-        return {
-          nodeLabels: true,
-          size: {
-            w: 1000,
-            h: 500
-          },
-          nodeSize: this.nodeSize,
-          force: 5000,
-          canvas: this.canvas
-        }
-      }
-    },
-    methods: {
-      linkInfo(link) {
-        if (link.index != null) this.tableLinkInformation.push(link)
-
-        link._svgAttrs = {
-          'marker-end': 'url(#m-end)',
-          'marker-start': 'url(#m-start)'
-        }
-        return link
-      },
-      nodeInfo(event, node) {
-        this.baseTable = 0
-        this.derivedTable = 0
-
-
-        this.tableLinkInformation.forEach(element => {
-          if (element.sid == node.id) {
-            this.baseTable += 1
-          }
-
-          if (element.tid == node.id) {
-            this.derivedTable += 1
-          }
-        })
-      }
-    },
-    created() {
-      // Retrieving all nodes from vuex
-      this.nodes = this.$store.getters.allNodes
-
-      // Retrieving all links from vuex
-      this.links = this.$store.getters.allLinks
     },
     components: {
-      D3Network,
       Navbar,
+      SQLInput,
+      ForceGraph,
       Footer
     }
   }
