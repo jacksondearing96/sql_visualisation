1624ff308086337c13dccc9fc8f624f06de10cc3
Gabriel Piragibe <gabriel.piragibe@hotmail.com>
2020-08-31 09:09:37 +0930

    Complemented back-end code to support POST req

--- backend-Flask/Flask_main.py
+++ backend-Flask/Flask_main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS
+
+# Configuration
+DEBUG = True
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
 
 @app.route('/')
 def index():
     return render_template('upload.html')
+
 
 """
 Is called by the html template after file is uploaded.
@@ -12,10 +22,16 @@ The upload_file is just returning the uploaded file currently
 this will be passed to the sql script in the future
 which will then be redirected to the vue application.
 """
+
+
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
+    response_object = {'status': 'success'}
     if request.method == "POST" and request.files:
-        return request.files["file"].read()
+        file = request.files['file']
+        response_object['name'] = file.filename
+    return jsonify(response_object)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
