b88e489caec19f29b38ed5d47abb6d1495a63002
Jack <jacksondearing96@.gmail.com>
2020-09-03 00:19:02 +0930

    Added node type to PrettyPrinter tables

--- back-end/src/main/java/PrettyPrinter.java
+++ back-end/src/main/java/PrettyPrinter.java
@@ -1,25 +1,45 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
+/**
+ * Printer class for printing readable tables to the terminal.
+ */
 public class PrettyPrinter {
-    private static void horizontalLine(int maxWidth) {
+
+    public static void printLineageNodesNames(ArrayList<LineageNode> list) {
+        for (LineageNode item : list) System.out.print(item.getName() + ", ");
+        System.out.print("\n");
+    }
+
+    public static void printColumns(ArrayList<Column> list) {
+        for (Column item : list) System.out.print(item.getName() + ", ");
+        System.out.print("\n");
+    }
+
+    private static void printHorizontalLine(int maxWidth) {
         System.out.print("+");
         for (int i = 0; i < maxWidth + 2; ++i) System.out.print("-");
         System.out.print("+\n");
     }
 
     private static void printLineContent(int maxWidth, String content) {
+        // Left margin.
         System.out.print("| " + content);
+
         int remainingSpaces = maxWidth - content.length();
         for (int i = 0; i < remainingSpaces; ++i) System.out.print(" ");
+
+        // Right margin.
         System.out.print(" |\n");
     }
 
-    public static void print(LineageNode lineageNode) {
+    public static void printLineageNode(LineageNode lineageNode) {
         ArrayList<String> rows = new ArrayList<>();
         int maxWidth = 0;
 
         String header = lineageNode.getName();
         if (lineageNode.hasAlias()) header += " AS " + lineageNode.getAlias();
+        header += " TYPE(" + lineageNode.getType() + ")";
         if (header.length() > maxWidth) maxWidth = header.length();
 
         for (Column column : lineageNode.getColumns()) {
@@ -30,11 +50,12 @@ public class PrettyPrinter {
             rows.add(row);
         }
 
-        System.out.println("\n\n");
-        horizontalLine(maxWidth);
+        // Table structure.
+        System.out.println("\n");
+        printHorizontalLine(maxWidth);
         printLineContent(maxWidth, header);
-        horizontalLine(maxWidth);
+        printHorizontalLine(maxWidth);
         for (String row : rows) printLineContent(maxWidth, row);
-        horizontalLine(maxWidth);
+        printHorizontalLine(maxWidth);
     }
 }
