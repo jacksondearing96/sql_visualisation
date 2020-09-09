ae0b258d099ddf24ff84e618e97142814318a845
Jack <jacksondearing96@.gmail.com>
2020-09-09 16:05:06 +0930

    Accounted for wildcard operators as select items

--- back-end/src/main/java/SivtVisitor.java
+++ back-end/src/main/java/SivtVisitor.java
@@ -368,6 +368,16 @@ class SivtVisitor<R, C> extends AstVisitor<R, C> {
      */
     @Override
     public final R visitNode(Node node, C context) {
+
+        // Columns are usually captured in the visitIdentifier function but wildcard operators
+        // are not classed as an Identifier which means they are skipped.
+        // Explicitly add wildcard select items here instead.
+        if (node.toString().equals("*")) {
+            if (!currentlyInside.isEmpty() && currentlyInside.peek() == SelectItem.class) {
+                columnsStack.peek().add(new Column(node.toString()));
+            }
+        }
+
         List<? extends Node> children = node.getChildren();
         for (Node child : children) {
             this.process(child, context);
