38a37a52d7a265e5593894a28c08ab2297b0a452
Jack <jacksondearing96@.gmail.com>
2020-09-29 00:03:29 +0930

    Server changes

--- back-end/server/server.py
+++ back-end/server/server.py
@@ -2,20 +2,23 @@ from flask import Flask, render_template, request
 import os
 from flask_cors import CORS
 import jnius_config
-from jnius import autoclass
 import json
 
 
 with open('./configuration/config.JSON') as config_file:
     data = json.load(config_file)
 
-jnius_config.set_classpath('.', data['parser'])
+jnius_config.set_classpath('.', data['lineageExtractor'])
+from jnius import autoclass
 
 app = Flask(__name__, root_path='../../front-end/')
 
 
 @app.route('/')
 def index():
+    lineageExtractor = autoclass(data['lineageExtractorClasses'])
+    response_text = lineageExtractor.extractLineageAsJson('SELECT a, b FROM c###')
+    print(response_text)
     return render_template('index.html')
 
 
