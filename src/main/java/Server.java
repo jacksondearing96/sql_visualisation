d968e14b9c640d89f9ef4b25100c4c5e6ff0a556
Jack <jacksondearing96@.gmail.com>
2020-09-29 20:45:09 +0930

    Java server and directory restructure

---
+++ src/main/java/Server.java
@@ -0,0 +1,13 @@
+import static spark.Spark.*;
+
+public class Server {
+    public static void main(String[] args) {
+        staticFiles.location("public");
+        get("/", (request, response) -> "index.html");
+
+        post("/upload_files", (request, response) -> {
+            System.out.println(request.body());
+            return "post method returning";
+        });
+    }
+}
