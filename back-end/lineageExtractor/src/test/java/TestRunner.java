fefd8424e1a731d4874160082f72a0ec0f34eb25
Josh Tatton <7052428+JTatton@users.noreply.github.com>
2020-09-30 13:01:38 +0930

    Breaking changes! new test runner

--- back-end/lineageExtractor/src/test/java/TestRunner.java
+++ back-end/lineageExtractor/src/test/java/TestRunner.java
@@ -1,104 +1,16 @@
-import org.junit.jupiter.api.*;
 import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
-public class TestRunner {
+public class TestRunner{
 
-    @BeforeAll
-    static void setup() {
-        System.out.println("Testing for SIVT Back-end:");
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(TestSuite.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        System.out.println(result.wasSuccessful());
     }
-
-    @AfterAll
-    static void afterAll() {
-        System.out.println("All tests complete.");
-    }
-
-    @BeforeEach
-    void beforeEach(TestInfo testInfo) {
-        System.out.println("Testing: " + testInfo.getDisplayName() + " - Started");
-    }
-
-    @AfterEach
-    void afterEach(TestInfo testInfo) {
-        System.out.println("Testing: " + testInfo.getDisplayName() + " - Complete");
-    }
-
-    @Test
-    void testCreates(){
-        JUnitCore.runClasses(TestCreate.class);
-    }
-
-    @Test
-    @DisplayName("testInserts")
-    void testInserts(){
-        JUnitCore.runClasses(TestInsert.class);
-    }
-
-    @Test
-    @DisplayName("testAlias")
-    void testAlias(){
-        JUnitCore.runClasses(TestAlias.class);
-    }
-
-    @Test
-    @DisplayName("testConditionals")
-    void testConditionals(){
-        JUnitCore.runClasses(TestConditional.class);
-    }
-
-    @Test
-    @DisplayName("testFiles")
-    void testFiles(){
-        JUnitCore.runClasses(TestFiles.class);
-    }
-
-    @Test
-    @DisplayName("testLiterals")
-    void testLiterals(){
-        JUnitCore.runClasses(TestLiteral.class);
-    }
-
-    @Test
-    @DisplayName("testColumns")
-    void testColumns(){
-        JUnitCore.runClasses(TestColumns.class);
-    }
-
-    @Test
-    @DisplayName("testWildcards")
-    void testWildcards(){
-        JUnitCore.runClasses(TestWildcard.class);
-    }
-
-    @Test
-    @DisplayName("testPrepares")
-    void testPrepares(){
-        JUnitCore.runClasses(TestPrepare.class);
-    }
-
-    @Test
-    @DisplayName("testMultiple")
-    void testMultiples(){
-        JUnitCore.runClasses(TestMultiple.class);
-    }
-
-    @Test
-    @DisplayName("testAnonymous")
-    void testAnonymous(){
-        JUnitCore.runClasses(TestAnonymous.class);
-    }
-
-    @Test
-    @DisplayName("testLineage")
-    void testLineage(){
-        JUnitCore.runClasses(TestLineage.class);
-    }
-
-    @Test
-    @DisplayName("testMisc")
-    void testMisc(){
-        JUnitCore.runClasses(TestMisc.class);
-    }
-
 
 }
